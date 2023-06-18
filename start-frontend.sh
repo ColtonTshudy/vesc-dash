a5a01eace8f2dd8b8621502b0f1455cb7efaddf3
ColtonTshudy <coltont@vt.edu>
2023-06-18 18:07:19 -0400

    Added unclutter (cursor removal) to start bash

--- start-frontend.sh
+++ start-frontend.sh
@@ -1,2 +1,3 @@
 (cd /home/tomos/Projects/VESCDash/vesc-dash/build ;sudo -H -u tomos /bin/python -m http.server 5051) &
 DISPLAY=:0 firefox --kiosk http://localhost:5051
+DISPLAY=:0 unclutter -idle 0.1
