c5e9534cede6770153de11ea9eaefac6ec4a885b
ColtonTshudy <coltont@vt.edu>
2023-06-18 18:37:42 -0400

    Added electron

--- start-frontend.sh
+++ start-frontend.sh
@@ -1,3 +1,4 @@
 (cd /home/tomos/Projects/VESCDash/vesc-dash/build ;sudo -H -u tomos /bin/python -m http.server 5051) &
 DISPLAY=:0 xdotool mousemove 2000 2000
-DISPLAY=:0 firefox --kiosk http://localhost:5051
+DISPLAY=:0 ./Electron.js
+# DISPLAY=:0 firefox --kiosk http://localhost:5051
