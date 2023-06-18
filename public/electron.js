fae523a08ddeae4a834ba7310eb6ea715f519d2a
ColtonTshudy <coltont@vt.edu>
2023-06-18 18:50:44 -0400

    Added kiosk mode to electron, added electron to start bash

--- public/electron.js
+++ public/electron.js
@@ -10,6 +10,8 @@ function createWindow() {
         width: 1024,
         height: 600,
         webPreferences: { nodeIntegration: true, contextIsolation: false },
+        kiosk: true,
+        frame: false,
     });
     // and load the index.html of the app.
     console.log(__dirname);
